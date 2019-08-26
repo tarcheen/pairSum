6b5ae489c94fe724fa511e478afb30cb13669c4b
Hamed <hamed0311@gmail.com>
2019-08-25 19:24:13 -0700

    initial

--- pairSum.cpp
+++ pairSum.cpp
@@ -1,1 +1,17 @@
+/*
+Developer: Hamed Mirlohi
+Date: Aug 25 2019
+In this program we want to find all pairs of numbers which sums to a total given. for example 
+{4,1,-1,7,3}    total = 5
+result = (4,1)
 
+Note: you are not allowed to use the brute force way to calculate the pair since the complexity is O(n^2)
+ */
+
+#include <iostream>
+
+int main(void)
+{
+    int arr[] = {1 ,5 ,7 ,-1 ,50 };
+    int total = 6;
+}
